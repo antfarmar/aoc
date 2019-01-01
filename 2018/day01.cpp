8b49b9d4861ba0b2ebe783c10859ad4b2b99d7ee
antfarmar <markfarrugiamail@gmail.com>
2018-12-31 20:32:04 -0500

    CRLF to LF (dos2unix)

--- 2018/day01.cpp
+++ 2018/day01.cpp
@@ -1,6 +1,6 @@
 // Advent of Code 2018
 // Day 1: Chronal Calibration
-
+// https://adventofcode.com/2018/day/1
 #include <iostream>
 #include <iterator>
 #include <numeric>
@@ -8,12 +8,12 @@
 #include <vector>
 
 // Sum the frequency data.
-int part1(std::vector<int> &data) {
+int part1(std::vector<int>& data) {
     return std::accumulate(data.begin(), data.end(), 0);
 }
 
 // Cycle through the frequencies until we get a sum match.
-int part2(std::vector<int> &data) {
+int part2(std::vector<int>& data) {
     int i{0}, sum{0}, size = data.size();
     std::unordered_set<int> memo{};
     while (memo.insert(sum += data[i++ % size]).second)
@@ -24,6 +24,6 @@ int part2(std::vector<int> &data) {
 // Parse the frequency data and solve.
 int main() {
     std::vector<int> data{std::istream_iterator<int>{std::cin}, {}};
-    std::cout << "Part 1: Sum = " << part1(data) << std::endl; // 520
-    std::cout << "Part 2: Hit = " << part2(data) << std::endl; // 394
+    std::cout << "Part 1: Sum = " << part1(data) << std::endl;  // 520
+    std::cout << "Part 2: Hit = " << part2(data) << std::endl;  // 394
 }
