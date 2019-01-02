b1673e21a7aea71abce89cd47995ea61f808ae09
antfarmar <markfarrugiamail@gmail.com>
2019-01-01 23:53:15 -0500

    added link to website

--- 2018/day02.cpp
+++ 2018/day02.cpp
@@ -1,5 +1,6 @@
 // Advent of Code 2018
 // Day 2: Inventory Management System
+// https://adventofcode.com/2018/day/2
 
 #include <algorithm>
 #include <iostream>
@@ -10,12 +11,12 @@
 #include <vector>
 
 // Count box id's with 2 and 3 of any letter.
-int part1(std::vector<std::string> &data) {
+int part1(std::vector<std::string>& data) {
     int dubs{0}, trip{0};
-    for (std::string &line : data) {
+    for (std::string& line : data) {
         int cnt[26]{0};
         std::for_each(line.begin(), line.end(),
-                      [&cnt](char &c) { ++cnt[c - 'a']; });
+                      [&cnt](char& c) { ++cnt[c - 'a']; });
         dubs += std::any_of(std::begin(cnt), std::end(cnt),
                             [](int n) { return n == 2; })
                     ? 1
@@ -30,7 +31,7 @@ int part1(std::vector<std::string> &data) {
 
 // Find the 2 box id's that differ by 1 character.
 // Compare every id pair: O(n^2)
-std::string part21(std::vector<std::string> &data) {
+std::string part21(std::vector<std::string>& data) {
     for (auto line1 = data.begin(); line1 < data.end() - 1; ++line1) {
         for (auto line2 = line1 + 1; line2 < data.end(); ++line2) {
             if ([&line1, &line2](int diff) {
@@ -47,7 +48,7 @@ std::string part21(std::vector<std::string> &data) {
 
 // Find the 2 box id's that differ by 1 character.
 // Sort id's and hope they are adjacent: O(n) + O(n log n)
-std::string part22(std::vector<std::string> &data) {
+std::string part22(std::vector<std::string>& data) {
     std::sort(data.begin(), data.end());
     for (auto line1 = data.begin(); line1 < data.end() - 1; ++line1) {
         if ([&line1](int diff) {
@@ -65,10 +66,10 @@ std::string part22(std::vector<std::string> &data) {
 // Find the 2 box id's that differ by 1 character.
 // Hash every box id substring and wait for a match.
 // O(n*m) where m = string length of the ids, space complexity = O(n)
-std::string part23(std::vector<std::string> &data) {
+std::string part23(std::vector<std::string>& data) {
     for (size_t i = 0; i < data.size(); i++) {
         std::unordered_set<std::string> set;
-        for (auto &line : data) {
+        for (auto& line : data) {
             std::string sub = line.substr(0, i) + line.substr(i + 1);
             if (!set.insert(sub).second)
                 return sub;
@@ -81,7 +82,7 @@ std::string part23(std::vector<std::string> &data) {
 int main() {
     std::vector<std::string> data{std::istream_iterator<std::string>{std::cin},
                                   {}};
-    std::cout << "Part 1: Count = " << part1(data) << std::endl; // 6916
+    std::cout << "Part 1: Count = " << part1(data) << std::endl;  // 6916
     std::cout << "Part 2: Match = " << part23(data)
-              << std::endl; // oeylbtcxjqnzhgyylfapviusr
+              << std::endl;  // oeylbtcxjqnzhgyylfapviusr
 }
