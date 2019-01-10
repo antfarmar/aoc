3e9167c9ad667bfabd897e7ffd114302e7125118
antfarmar <markfarrugiamail@gmail.com>
2019-01-10 05:57:36 -0500

    added link removed includes

--- 2018/day08.cpp
+++ 2018/day08.cpp
@@ -1,53 +1,45 @@
 // Advent of Code 2018
 // Day 8: Memory Maneuver
-// #include <regex>
-// #include <array>
-// #include <unordered_set>
-// #include <unordered_map>
-// #include <string>
-// #include <deque>
-// #include <iterator>
-// #include <map>
-// #include <set>
+// https://adventofcode.com/2018/day/8
+
 #include <algorithm>
 #include <iostream>
 #include <numeric>
 #include <vector>
-// using namespace std;
 
-// Helper struct
+// node in a tree
 struct Node {
     std::vector<Node> children;
     std::vector<int> metadata;
 };
 
 // Recursively parse the input tree nodes starting at the root.
-std::istream &operator>>(std::istream &is, Node &node) {
+std::istream& operator>>(std::istream& is, Node& node) {
     int childCnt, metadataCnt;
     is >> childCnt >> metadataCnt;
     node.children.resize(childCnt);
     node.metadata.resize(metadataCnt);
-    for (auto &child : node.children)
+    for (auto& child : node.children)
         is >> child;
-    for (auto &data : node.metadata)
+    for (auto& data : node.metadata)
         is >> data;
     return is;
 }
 
 // part 1: Recursively traverse the tree and sum each node's metadata.
-int sumMetadata(const Node &node) {
+int sumMetadata(const Node& node) {
     int sum = std::accumulate(node.metadata.begin(), node.metadata.end(), 0);
-    for (auto &child : node.children)
+    for (auto& child : node.children)
         sum += sumMetadata(child);
     return sum;
 }
 
 // Part 2: Recursively traverse the tree, sum each node's metadata + children.
-int rootValue(const Node &node) {
+int rootValue(const Node& node) {
     int rootSum{0}, off{-1};
     if (node.children.empty())
         return sumMetadata(node);
-    for (auto &idx : node.metadata) // data = index to child
+    for (auto& idx : node.metadata)  // data = index to child
         if (size_t(idx + off) < node.children.size())
             rootSum += rootValue(node.children[idx + off]);
     return rootSum;
