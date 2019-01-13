1c1621dbb754e7b24d78898436f01b9f5b3a8200
antfarmar <markfarrugiamail@gmail.com>
2019-01-13 17:59:54 -0500

    copy replaced

--- 2018/original/day25.cpp
+++ 2018/original/day25.cpp
@@ -1,109 +1,71 @@
 // Advent of Code 2018
-// Day 00:
-// #include <bits/stdc++.h>
-// #include <array>
-// #include <bitset>
-// #include <cassert>
-// #include <deque>
-// #include <list>
-// #include <map>
-// #include <numeric>
-// #include <regex>
-// #include <set>
-// #include <sstream>
-// #include <string>
-// #include <tuple>
-// #include <unordered_map>
-// #include <unordered_set>
-#include <algorithm>
+// Day 25: Four-Dimensional Adventure
+// https://adventofcode.com/2018/day/25
+
 #include <chrono>
+#include <functional>
 #include <iostream>
-#include <istream>
 #include <iterator>
 #include <vector>
-using namespace std;
 
-// Test suite of examples
-void test(bool doTest = false) {
-    if (!doTest)
-        return;
-    cerr << "Testing..." << endl;
-
-    // // suite of given sample examples
-    // typedef pair<string, int> in_out;
-    // vector<in_out> suite = {};
-
-    // // test the testsuite
-    // for (auto const &[input, output] : suite) {
-    //     auto got = fn();
-    //     if (got != output) {
-    //         cerr << "For: " << input << endl;
-    //         cerr << "Exp: " << output << endl;
-    //         cerr << "Got: " << got << endl;
-    //         abort();
-    //     }
-    // }
-}
-
-// std::istream &operator>>(std::istream &is, T &t) {
-//     int ig = 99;
-//     char ic;
-//     cin.ignore(ig, '#');
-//     is >> t >> ic;
-//     return is;
-// }
-
-// Parse input via sscanf.
-// vector<int> readInput() {
-//     vector<int> v;
-//     char form[] = "position=<%d>";
-//     for (string line; getline(cin, line);) {
-//         int n;
-//         sscanf(line.c_str(), form, &n);
-//         v.push_back(n);
-//     }
-//     return v;
-// }
-
-// manhattan distance
-// int64_t mhDist(const Point &a, const Point &b) {
-//     return abs(a.x - b.x) + abs(a.y - b.y) + abs(a.z - b.z);
-// }
-
-void solve() {
-    test();
-    // vector<string> data{istream_iterator<string>{cin}, {}};
-    // vector<int> data = readInput();
-
-    // int x;
-    // while (cin >> x) {
-    //     ;
-    // }
-
-    // for (string line; getline(cin, line);) {
-    //     ;
-    // }
-
+// a 4D point in spacetime
+struct Point {
     int x, y, z, t;
-    while (scanf("%d,%d,%d,%d", &x, &y, &z, &t) == 4) {
-        cerr << x;
-    }
+};
 
-    cout << "[Part 01] = " << 1 << endl;
-    cout << "[Part 02] = " << 2 << endl;
+// parse the points via an istream
+std::istream& operator>>(std::istream& is, Point& p) {
+    char comma;  // x,y,z,t
+    is >> p.x >> comma >> p.y >> comma >> p.z >> comma >> p.t;
+    return is;
 }
 
-// Main: Time the solver.
+// manhattan distance b/w two 4D points
+int mhdist(const Point& p, const Point& q) {
+    return abs(p.x - q.x) + abs(p.y - q.y) + abs(p.z - q.z) + abs(p.t - q.t);
+}
+
+// dfs on graph of adjacency lists of points within distance 3 of each other
+void solve() {
+    const std::vector<Point> points{std::istream_iterator<Point>{std::cin}, {}};
+    const int pointCount = points.size();
+
+    std::vector<std::vector<int>> within3(pointCount);
+    std::vector<bool> visited(pointCount);
+
+    // for each point pair, build an adjacency list of points within dist 3
+    for (int p = 0; p < pointCount; ++p)
+        for (int q = p + 1; q < pointCount; ++q)
+            if (mhdist(points[p], points[q]) <= 3)
+                within3[p].push_back(q), within3[q].push_back(p);
+
+    // recursive lambda: depth-first search to mark nodes as seen
+    const std::function<void(int)> visit_dfs = [&](const int node) {
+        visited[node] = true;
+        for (const int edge : within3[node])
+            if (not visited[edge])
+                visit_dfs(edge);
+    };
+
+    // count constellations of points and mark them as counted
+    int constellations = 0;
+    for (int point = 0; point < pointCount; ++point)
+        if (not visited[point])
+            ++constellations, visit_dfs(point);
+
+    // Part 1
+    // How many constellations are formed by the fixed points in spacetime?
+    // Your puzzle answer was 324
+    std::cout << "Part 1: " << constellations << std::endl;
+}
+
+// time the solver
 int main() {
-    // Speed up C++ io by unsyncing with C stdio and untie cin/cout.
-    ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
-    // Time the solve() function in milliseconds.
-    auto start_time = std::chrono::high_resolution_clock::now();
+    auto start_time = std::chrono::steady_clock::now();
     solve();
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto ms_count = std::chrono::duration_cast<std::chrono::milliseconds>(
-                        end_time - start_time)
-                        .count();
-    std::clog << "[Runtime] = " << ms_count << "ms" << std::endl;
+    auto end_time = std::chrono::steady_clock::now();
+    auto ms = std::chrono::duration<double, std::milli>(end_time - start_time);
+    std::clog << "[Runtime: " << ms.count() << "ms]" << std::endl;
 }
